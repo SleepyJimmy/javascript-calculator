b89f22d51842ffec721697eaab9ab3c3dd0fa3fb
SleepyJimmy <lijunfang97@gmail.com>
2024-09-23 17:21:18 +0100

    Added logic where inputting numbers into the calculator will correctly show on the display.

--- javascript.js
+++ javascript.js
@@ -8,36 +8,48 @@ const numbers_arr = ["AC", "+/-", "\%", "1", "2", "3", "4", "5", "6", "7", "8", 
 const numbers_bot_arr = ["0", "."];
 const operator_arr = ["+", "-", "*", "/", "="];
 
-// create divs for top numbers container
-for (let i = 0; i < 12; i++) {
-    let button = document.createElement("button");
-    button.textContent = numbers_arr[i];
-    numbers_top.appendChild(button);
-}
+const cache = [];
 
-// create divs for bottom numbers container
-for (let i = 0; i < 2; i++) {
-    let className = `num_${numbers_bot_arr[i].replace('.', 'dot')}`;
-    let button = document.createElement("button");
-    button.textContent = numbers_bot_arr[i];
-    button.classList.add(className);
-    numbers_bot.appendChild(button);
-}
+// create divs for buttons
+function createDivs() {
+    // create divs for top numbers container
+    for (let i = 0; i < 12; i++) {
+        let button = document.createElement("button");
+        button.textContent = numbers_arr[i];
+        numbers_top.appendChild(button);
+    }
 
-// create divs for operators container
-for (let i = 0; i < 5; i++) {
-    let button = document.createElement("button");
-    button.textContent = operator_arr[i];
-    operators.appendChild(button);
-}
+    // create divs for bottom numbers container
+    for (let i = 0; i < 2; i++) {
+        let className = `num_${numbers_bot_arr[i].replace('.', 'dot')}`;
+        let button = document.createElement("button");
+        button.textContent = numbers_bot_arr[i];
+        button.classList.add(className);
+        numbers_bot.appendChild(button);
+    }
+
+    // create divs for operators container
+    for (let i = 0; i < 5; i++) {
+        let button = document.createElement("button");
+        button.textContent = operator_arr[i];
+        operators.appendChild(button);
+    }
+};
+createDivs();
+
 
 const display = document.querySelector(".display");
+display.textContent = "0";
 function updateDisplay() {
-    display.textContent += humanInput;
+    if (display.textContent == 0 && (Number.isInteger(parseInt(humanInput)))) {
+        display.textContent = humanInput;
+    } else {
+        display.textContent += humanInput;
+    }
 }
 function clearDisplay() {
     humanInput = "";
-    display.textContent = "";
+    display.textContent = "0";
     updateDisplay();
 }
 
@@ -47,11 +59,25 @@ buttons.forEach((button) => {
         if (button.textContent == "AC") {
             clearDisplay();
         } else if (button.textContent == "="){
-            // apply the operation here
+            operate();
+        } else if (operator_arr.includes(button.textContent)) {    // add current display into cache when user clicks an operator
+            let operator = button.textContent
+            
+            cache.push(parseInt(display.textContent))
+            if (cache.length == 3) {
+                operate();
+            }
+            cache.push(operator)
+            console.log(cache);
         } else {
-            humanInput = button.textContent;
-            updateDisplay();
+            if (display.textContent.length < 8) {
+                humanInput = button.textContent;
+                updateDisplay();    
+                console.log(humanInput);
+            }
         }
+        
+        
     })
 })
 
@@ -59,3 +85,4 @@ buttons.forEach((button) => {
 function operate() {
 
 }
+
